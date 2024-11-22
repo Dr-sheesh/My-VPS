ee339d86654a623a8fea1840abe3e496d4ea176f
Dr. Sheesh <mamasboy199329@Gmail.com>
2024-11-22 19:48:50 +0000

    a

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.git
 sudo apt update
 sudo apt install playit
 sudo apt install openjdk-21-jdk
-sudo JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/bin/java
+JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/bin/java
